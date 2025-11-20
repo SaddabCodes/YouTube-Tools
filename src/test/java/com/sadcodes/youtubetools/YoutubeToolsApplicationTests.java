67ffbcf528955835d486799b1b7a404960eaba21
saddab <saddabakhtar17@gmil.com>
2025-11-20 15:21:28 +0530

    create YouTube Tools Project

---
+++ src/test/java/com/sadcodes/youtubetools/YoutubeToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadcodes.youtubetools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
