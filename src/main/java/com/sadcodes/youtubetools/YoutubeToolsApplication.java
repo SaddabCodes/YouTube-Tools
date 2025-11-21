97f516b6d100c69b839eaf1672ed5bfa0d4bb99f
saddab <saddabakhtar17@gmil.com>
2025-11-21 15:29:01 +0530

    fix errors

--- src/main/java/com/sadcodes/youtubetools/YoutubeToolsApplication.java
+++ src/main/java/com/sadcodes/youtubetools/YoutubeToolsApplication.java
@@ -8,6 +8,7 @@ public class YoutubeToolsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(YoutubeToolsApplication.class, args);
+
     }
 
 }
--- src/main/java/com/sadcodes/youtubetools/controller/ThumbnailController.java
+++ src/main/java/com/sadcodes/youtubetools/controller/ThumbnailController.java
@@ -1,13 +1,39 @@
 package com.sadcodes.youtubetools.controller;
 
+import com.sadcodes.youtubetools.service.ThumbnailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ThumbnailController {
 
+    @Autowired
+    private ThumbnailsService thumbnailsService;
+
     @GetMapping("/thumbnail")
-    public String getThumbnails(){
+    public String getThumbnails() {
         return "thumbnails";
     }
+
+    @PostMapping("/get-thumbnail")
+    public String showThumbnails(@RequestParam("videoUrlOrId") String videoUrlOrId, Model model) {
+
+        String videoId = thumbnailsService.extractVideoId(videoUrlOrId);
+        if (videoId == null) {
+            model.addAttribute("error", "Invalid Youtube URL");
+            return "thumbnails";
+        }
+
+        String thumbnailUrl = "https://img.youtube.com/vi/" + videoId + "/maxresdefault.jpg";
+
+        model.addAttribute("videoId", videoId);
+        model.addAttribute("thumbnailUrl", thumbnailUrl);
+
+        return "thumbnails";
+    }
+
 }
